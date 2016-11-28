e861e568d0632f8830216f934d0220309ec2c342
RomarioGorilyi <rom4ik-007@yandex.ua>
2016-11-28 13:35:03 +0200

    refactor NoSorting behavior

--- src/main/java/ua/com/netcracker/training/lab00/sorting/NoSorting.java
+++ src/main/java/ua/com/netcracker/training/lab00/sorting/NoSorting.java
@@ -8,9 +8,6 @@ import ua.com.netcracker.training.lab00.annotation.Sorting;
 @Sorting(name = "No sort", type = "None")
 public class NoSorting implements SortingBehavior {
 
-    public NoSorting() {
-    }
-
     @Override
     public void sort(int[] array) {
         // leave our array without any modifications
