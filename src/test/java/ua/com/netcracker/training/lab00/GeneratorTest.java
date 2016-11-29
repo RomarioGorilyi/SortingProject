3875672bc09a75148013e33be602eb24d40bdb50
RomarioGorilyi <rom4ik-007@yandex.ua>
2016-11-29 02:33:06 +0200

    refactor unit tests

--- src/test/java/ua/com/netcracker/training/lab00/GeneratorTest.java
+++ src/test/java/ua/com/netcracker/training/lab00/GeneratorTest.java
@@ -1,5 +1,6 @@
 package ua.com.netcracker.training.lab00;
 
+import org.junit.Assert;
 import org.junit.Test;
 import ua.com.netcracker.training.lab00.generation.RandomGeneration;
 import ua.com.netcracker.training.lab00.generator.*;
@@ -10,39 +11,63 @@ import ua.com.netcracker.training.lab00.sorting.JavaSort;
  */
 public class GeneratorTest {
 
+    private static int expectedArraySize = 100;
+
+    private boolean isArrayAscending(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isArrayDescending(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] < array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     @Test
     public void testUnsortedGenerator() {
-        NumbersGenerator generator = new UnsortedGenerator(new RandomGeneration(), new JavaSort());
-        int[] numbers = generator.generateNumbers(10);
-        for (int number : numbers) {
-            System.out.println(number);
-        }
+        NumbersGenerator generator = new UnsortedGenerator(new RandomGeneration());
+        int[] numbers = generator.generateNumbers(expectedArraySize);
+
+        Assert.assertEquals(expectedArraySize, numbers.length);
     }
 
     @Test
     public void testAscendingGenerator() {
         NumbersGenerator generator = new AscendingGenerator(new RandomGeneration(), new JavaSort());
-        int[] numbers = generator.generateNumbers(10);
-        for (int number : numbers) {
-            System.out.println(number);
-        }
+        int[] numbers = generator.generateNumbers(expectedArraySize);
+
+        Assert.assertEquals(expectedArraySize, numbers.length);
+        Assert.assertTrue(isArrayAscending(numbers));
     }
 
     @Test
     public void testDescendingGenerator() {
         NumbersGenerator generator = new DescendingGenerator(new RandomGeneration(), new JavaSort());
-        int[] numbers = generator.generateNumbers(10);
-        for (int number : numbers) {
-            System.out.println(number);
-        }
+        int[] numbers = generator.generateNumbers(expectedArraySize);
+
+        Assert.assertEquals(expectedArraySize, numbers.length);
+        Assert.assertTrue(isArrayDescending(numbers));
     }
 
     @Test
     public void testAscendingWithLastRandomGenerator() {
         NumbersGenerator generator = new AscendingWithLastRandomGenerator(new RandomGeneration(), new JavaSort());
-        int[] numbers = generator.generateNumbers(10);
-        for (int number : numbers) {
-            System.out.println(number);
+        int[] numbers = generator.generateNumbers(expectedArraySize);
+
+        int[] ascendingNumbers = new int[numbers.length - 1];
+        for (int i = 0; i < numbers.length - 1; i++) {
+            ascendingNumbers[i] = numbers[i];
         }
+
+        Assert.assertEquals(expectedArraySize, numbers.length);
+        Assert.assertTrue(isArrayAscending(ascendingNumbers));
     }
 }
--- src/test/java/ua/com/netcracker/training/lab00/SortingTest.java
+++ src/test/java/ua/com/netcracker/training/lab00/SortingTest.java
@@ -1,67 +1,83 @@
 package ua.com.netcracker.training.lab00;
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 import ua.com.netcracker.training.lab00.sorting.*;
+
+import java.security.SecureRandom;
 
 /**
  * @author Horilyi
  */
 public class SortingTest {
 
-    @Test
-    public void testBubbleFloatingUpSort() {
-        int[] unsortedArray = {18, 5, -1, 0, 6, 2, 15};
+    private static int[] unsortedArray;
 
-        SortingBehavior sorting = new BubbleFloatingUpSort();
-        int[] sortedArray = sorting.findSortedArray(unsortedArray);
-        int[] expectedSortedArray = {-1, 0, 2, 5, 6, 15, 18};
-
-        Assert.assertArrayEquals(expectedSortedArray, sortedArray);
+    private boolean isArraySorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
     }
 
-    @Test
-    public void testBubbleFloatingUpSortWithAlreadySortedArray() {
-        int[] unsortedArray = {-1, 0, 2, 5, 6, 15, 18};
+    @BeforeClass
+    public static void generateUnsortedArray() {
+        unsortedArray = new int[100];
+        SecureRandom random = new SecureRandom();
+        for (int i = 0; i < 100; i++) {
+            unsortedArray[i] = random.nextInt();
+        }
+    }
 
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(10); // 10 seconds max per method tested
+
+    @Test
+    public void testBubbleFloatingUpSort() {
         SortingBehavior sorting = new BubbleFloatingUpSort();
         int[] sortedArray = sorting.findSortedArray(unsortedArray);
-        int[] expectedSortedArray = {-1, 0, 2, 5, 6, 15, 18};
 
-        Assert.assertArrayEquals(expectedSortedArray, sortedArray);
+        Assert.assertTrue(isArraySorted(sortedArray));
     }
 
     @Test
     public void testBubbleSinkingSort() {
-        int[] unsortedArray = {18, 5, -1, 0, 6, 2, 15};
-
         SortingBehavior sorting = new BubbleSinkingSort();
         int[] sortedArray = sorting.findSortedArray(unsortedArray);
-        int[] expectedSortedArray = {-1, 0, 2, 5, 6, 15, 18};
 
-        Assert.assertArrayEquals(expectedSortedArray, sortedArray);
+        Assert.assertTrue(isArraySorted(sortedArray));
     }
 
     @Test
     public void testMergeSort() {
-        int[] unsortedArray = {18, 5, -1, 0, 6, 2, 15};
+        SortingBehavior sorting = new MergeSort();
+        int[] sortedArray = sorting.findSortedArray(unsortedArray);
+
+        Assert.assertTrue(isArraySorted(sortedArray));
+    }
+
+    @Test
+    public void testMergeSortWithEmptyArray() {
+        int[] unsortedArray = {};
 
         SortingBehavior sorting = new MergeSort();
         int[] sortedArray = sorting.findSortedArray(unsortedArray);
-        int[] expectedSortedArray = {-1, 0, 2, 5, 6, 15, 18};
+        int[] expectedSortedArray = {};
 
         Assert.assertArrayEquals(expectedSortedArray, sortedArray);
     }
 
     @Test
     public void testQuickSort() {
-        int[] unsortedArray = {18, 5, -1, 0, 6, 2, 15};
-
         SortingBehavior sorting = new QuickSort();
         int[] sortedArray = sorting.findSortedArray(unsortedArray);
-        int[] expectedSortedArray = {-1, 0, 2, 5, 6, 15, 18};
 
-        Assert.assertArrayEquals(expectedSortedArray, sortedArray);
+        Assert.assertTrue(isArraySorted(sortedArray));
     }
 
     @Test
@@ -77,23 +93,9 @@ public class SortingTest {
 
     @Test
     public void testJavaSort() {
-        int[] unsortedArray = {18, 5, -1, 0, 6, 2, 15};
-
         SortingBehavior sorting = new JavaSort();
         int[] sortedArray = sorting.findSortedArray(unsortedArray);
-        int[] expectedSortedArray = {-1, 0, 2, 5, 6, 15, 18};
 
-        Assert.assertArrayEquals(expectedSortedArray, sortedArray);
-    }
-
-    @Test
-    public void testNoSorting() {
-        int[] unsortedArray = {18, 5, -1, 0, 6, 2, 15};
-
-        SortingBehavior sorting = new NoSorting();
-        int[] sortedArray = sorting.findSortedArray(unsortedArray);
-        int[] expectedSortedArray = {18, 5, -1, 0, 6, 2, 15};
-
-        Assert.assertArrayEquals(expectedSortedArray, sortedArray);
+        Assert.assertTrue(isArraySorted(sortedArray));
     }
 }
